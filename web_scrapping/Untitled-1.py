d83d0b3e04499286ae65720bbcf9dcdfc5561e14
_humesh7 <patilhumesh88@gmail.com>
2026-04-08 22:06:28 +0530

    nothing much just learning python

---
+++ web_scrapping/Untitled-1.py
@@ -0,0 +1,15 @@
+name="alicia"
+age=22
+is_student=True
+x=int("10")
+y=str(25)
+z=int(3.9)
+lst=list("abc")
+name="alice"
+greeting="hello"
+name=input("enter your name:")
+print("hello",name)
+age=int(input("enter your age:"))
+price=float(input("enter you price:"))
+for i in range (3):
+    print (i)
